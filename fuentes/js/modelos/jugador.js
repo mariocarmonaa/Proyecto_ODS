9cee570ebd4c50b35d561d56fa9ac51c52f14e3d
lorenzo <llozanog06@educarex.es>
2026-04-12 12:14:01 +0200

    TT1-REGISTRO_USUARIO-COMPLETADO

---
+++ fuentes/js/modelos/jugador.js
@@ -0,0 +1,15 @@
+class Jugador{
+    #nombre
+    #fecha
+    #genero
+    #descripcion
+    #animalesSeleccionados
+
+    constructor(nombre, fecha, genero, descripcion, animalesSeleccionados){
+        this.#nombre = nombre
+        this.#fecha = fecha
+        this.#genero = genero
+        this.#descripcion = descripcion
+        this.#animalesSeleccionados = animalesSeleccionados
+    }
+}
