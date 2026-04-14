26f9d4731d8e70507efe4584e874f9b508ee34c5
Izan Casa <icanof04@educarex.es>
2026-04-14 11:36:20 +0200

    Añado vista Estadistica

---
+++ fuentes/js/vistas/VistaEstadisticas.js
@@ -0,0 +1,27 @@
+
+export default class VistaEstadisticas {
+    constructor() {
+        this.contenedor = document.querySelector("#estadisticas")
+        this.botonActualizar = document.querySelector("#botonEstadisticas")
+    }
+
+    mostrar(jugadores) {
+        if (jugadores.length === 0) {
+            this.contenedor.innerHTML = "<p>Sin datos</p>"
+            return
+        }
+        let sumaEdades = 0
+        jugadores.forEach(j => {
+            sumaEdades += new Date().getFullYear() - new Date(j.fecha).getFullYear()
+        })
+        const media = (sumaEdades / jugadores.length).toFixed(1)
+        this.contenedor.innerHTML = `
+            <p>👥 Total jugadores: ${jugadores.length}</p>
+            <p>📊 Edad media: ${media} años</p>
+        `
+    }
+
+    alActualizar(callback) {
+        this.botonActualizar.addEventListener('click', callback)
+    }
+}
