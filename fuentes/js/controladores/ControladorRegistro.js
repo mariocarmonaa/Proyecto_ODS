f68b6b7666b461a0014c290ed716927f34da2602
Izan <icanof04@educarex.es>
2026-05-03 13:44:17 +0200

    nuevo controladorregistro corrigo minusculas

--- fuentes/js/controladores/ControladorRegistro.js
+++ fuentes/js/controladores/ControladorRegistro.js
@@ -1,32 +1,35 @@
-import VistaRegistro from "../vistas/VistaRegistro.js"
-import Jugador from "../modelos/Jugador.js"
-import ModeloJugador from "../modelos/ModeloJugador.js"
-class ControladorRegistro{
-    #vistaRegistro
-    #modeloJugador
+import VistaRegistro from '../vistas/vistaregistro.js'
+import Jugador from '../modelos/jugador.js'
+import ModeloJugador from '../modelos/modelojugador.js'
 
-    constructor(){
-        console.log('Controlador en el aire')
-        this.#vistaRegistro = new VistaRegistro(this)
-        this.#modeloJugador = new ModeloJugador()
+class ControladorRegistro {
+    #vista
+    #modelo
+
+    constructor() {
+        this.#vista = new VistaRegistro(this)
+        this.#modelo = new ModeloJugador()
     }
 
-    insertar(datos){
-        const jugador = new Jugador(datos.nombre, datos.fechaNacimiento, datos.genero, datos.descripcion, datos.animales, datos.id)
-        this.#modeloJugador.agregarJugador(jugador)
-        this.#listar()
-    }
-    #listar(){
-        const datos = this.#modeloJugador.listar()
-        this.#vistaRegistro.listar(datos)
+    insertar(datos) {
+        this.#modelo.agregar(new Jugador(datos.nombre, datos.fechaNacimiento, datos.genero, datos.descripcion, datos.animales, datos.id))
+        this.#vista.listar(this.#modelo.listar())
     }
 
-    eliminar(id){
-        this.#modeloJugador.eliminarJugador(id)
-        this.#listar()
+    editar(datos) {
+        this.#modelo.editar(new Jugador(datos.nombre, datos.fechaNacimiento, datos.genero, datos.descripcion, datos.animales, datos.id))
+        this.#vista.listar(this.#modelo.listar())
+    }
+
+    eliminar(id) {
+        this.#modelo.eliminar(id)
+        this.#vista.listar(this.#modelo.listar())
+    }
+
+    prepararEdicion(id) {
+        const jugador = this.#modelo.listar().find(j => j.getId() === id)
+        if (jugador) this.#vista.rellenarFormulario(jugador)
     }
 }
 
-document.addEventListener('DOMContentLoaded', () =>{
-    new ControladorRegistro()
-})
+document.addEventListener('DOMContentLoaded', () => new ControladorRegistro())
