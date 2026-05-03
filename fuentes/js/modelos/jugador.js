42089330c0342433d2fa4a2411c5164de1c176a2
Izan <icanof04@educarex.es>
2026-05-03 14:01:49 +0200

    Refactor Jugador class to use public properties

--- fuentes/js/modelos/jugador.js
+++ fuentes/js/modelos/jugador.js
@@ -1,36 +1,20 @@
-export default class Jugador{
-    #nombre
-    #fecha
-    #genero
-    #descripcion
-    #animales
-    #id
-
-    constructor(nombre, fecha, genero, descripcion, animales, id){
-        this.#nombre = nombre
-        this.#fecha = new Date(fecha)
-        this.#genero = genero
-        this.#descripcion = descripcion
-        this.#animales = animales
-        this.#id = id
+export default class Jugador {
+    constructor(nombre, fecha, genero, descripcion, animales, id) {
+        this.nombre = nombre
+        this.fecha = new Date(fecha)
+        this.genero = genero
+        this.descripcion = descripcion
+        this.animales = animales
+        this.id = Number(id)
     }
 
-    getNombre(){
-        return this.#nombre
+    getNombre() { return this.nombre }
+
+    getFecha() {
+        return this.fecha.toLocaleDateString()
     }
 
-    getFecha(){
-        let fechaESP = this.#fecha.getDate()
-        fechaESP += '/' + (this.#fecha.getMonth() + 1)
-        fechaESP += '/' + this.#fecha.getFullYear()
-        return fechaESP
-    }
+    getGenero() { return this.genero }
 
-    getGenero(){
-        return this.#genero
-    }
-
-    getId(){
-        return this.#id
-    }
+    getId() { return this.id }
 }
