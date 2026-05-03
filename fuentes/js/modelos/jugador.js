f522e6a66faf6c3134f132f734619a07ffc80f78
Izan <icanof04@educarex.es>
2026-05-03 14:07:26 +0200

    Refactor Jugador class to simplify fecha handling
    Updated the fecha property to store the date as a string instead of a Date object. Removed unused getter methods for nombre, fecha, genero, and id.

--- fuentes/js/modelos/jugador.js
+++ fuentes/js/modelos/jugador.js
@@ -1,20 +1,10 @@
 export default class Jugador {
     constructor(nombre, fecha, genero, descripcion, animales, id) {
         this.nombre = nombre
-        this.fecha = new Date(fecha)
+        this.fecha = fecha
         this.genero = genero
         this.descripcion = descripcion
         this.animales = animales
         this.id = Number(id)
     }
-
-    getNombre() { return this.nombre }
-
-    getFecha() {
-        return this.fecha.toLocaleDateString()
-    }
-
-    getGenero() { return this.genero }
-
-    getId() { return this.id }
 }
