de5a67750927a011cee0ab0e4c9fc5213e2f677c
Izan <icanof04@educarex.es>
2026-05-03 13:56:47 +0200

    Refactor ControladorRegistro

--- fuentes/js/controladores/controladorregistro.js
+++ fuentes/js/controladores/controladorregistro.js
@@ -3,32 +3,47 @@ import Jugador from '../modelos/jugador.js'
 import ModeloJugador from '../modelos/modelojugador.js'
 
 class ControladorRegistro {
-    #vista
-    #modelo
-
     constructor() {
-        this.#vista = new VistaRegistro(this)
-        this.#modelo = new ModeloJugador()
+        this.vista = new VistaRegistro(this)
+        this.modelo = new ModeloJugador()
     }
 
     insertar(datos) {
-        this.#modelo.agregar(new Jugador(datos.nombre, datos.fechaNacimiento, datos.genero, datos.descripcion, datos.animales, datos.id))
-        this.#vista.listar(this.#modelo.listar())
+        this.modelo.agregar(
+            new Jugador(
+                datos.nombre,
+                datos.fechaNacimiento,
+                datos.genero,
+                datos.descripcion,
+                datos.animales,
+                datos.id
+            )
+        )
+        this.vista.listar(this.modelo.listar())
     }
 
     editar(datos) {
-        this.#modelo.editar(new Jugador(datos.nombre, datos.fechaNacimiento, datos.genero, datos.descripcion, datos.animales, datos.id))
-        this.#vista.listar(this.#modelo.listar())
+        this.modelo.editar(
+            new Jugador(
+                datos.nombre,
+                datos.fechaNacimiento,
+                datos.genero,
+                datos.descripcion,
+                datos.animales,
+                datos.id
+            )
+        )
+        this.vista.listar(this.modelo.listar())
     }
 
     eliminar(id) {
-        this.#modelo.eliminar(id)
-        this.#vista.listar(this.#modelo.listar())
+        this.modelo.eliminar(id)
+        this.vista.listar(this.modelo.listar())
     }
 
     prepararEdicion(id) {
-        const jugador = this.#modelo.listar().find(j => j.getId() === id)
-        if (jugador) this.#vista.rellenarFormulario(jugador)
+        const j = this.modelo.listar().find(j => j.getId() === id)
+        if (j) this.vista.rellenarFormulario(j)
     }
 }
 
